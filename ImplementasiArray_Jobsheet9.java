1a0374153ebf9d0b8f2dd6362537c9979af001d4
Yusra Yusuf <yusrayusuf811@gmail.com>
2023-10-29 22:55:18 +0700

    Tugas Kelompok

--- ImplementasiArray_Jobsheet9.java
+++ ImplementasiArray_Jobsheet9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tugaskelarray {
+public class ImplementasiArray_Jobsheet9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
